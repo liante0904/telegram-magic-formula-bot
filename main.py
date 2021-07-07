4d40bda0061ca885f064dc725e95d7093f02f2a5
seunghoon-shin <liante0904@gmail.com>
2021-07-07 10:16:41 +0900

    Edit ROE Bug Fix.

--- main.py
+++ main.py
@@ -435,7 +435,7 @@ def excel_write_row(*args):
     cell = write_ws.cell(nRowIdx, 6, data_Pbr)
     cell.number_format = '#,##0.00'
 
-    if data_Roe not in ('', '-') and isinstance(data_Roe, str): data_Roe = float(data_Roe.replace(',',''))
+    if data_Roe not in ('', '-') and not isinstance(data_Roe, str): data_Roe = float(data_Roe.replace(',',''))
 
     cell = write_ws.cell(nRowIdx, 7, data_Roe)
     cell.number_format = '#,##0.00'
